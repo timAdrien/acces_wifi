e4d7b714c3cb78c11284b9b8200b125717af041e
Timothée <Timothe@192.168.1.21>
2015-09-18 06:01:13 +0200

    essaies recuperation info dans BDD OK

--- user_connection.php
+++ user_connection.php
@@ -1,44 +1,32 @@
 <?php
+try{
+//on se connecte à la base de données:
+$connec = new PDO('mysql:host=127.0.0.1; dbname=db_acces_wifi', 'timyo', '123456');
+//$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}catch (PDOException $e){
+    echo 'PB de connection à la BDD';
+}
 
-session_start(); // à mettre tout en haut du fichier .php, cette fonction propre à PHP servira à maintenir la $_SESSION
-if(isset($_POST['connexion'])) { // si le bouton "Connexion" est appuyé
-    // on vérifie que le champ "Pseudo" n'est pas vide
-    // empty vérifie à la fois si le champ est vide et si le champ existe belle et bien (is set)
-    if(empty($_POST['mail'])) {
-        echo "Le champ Pseudo est vide.";
+$password = 'bite';
+
+$req = 'SELECT name';
+$req .= 'FROM user';
+$req .= 'WHERE firstname = "Yoann" ';
+//$req .= $password;
+try{
+$res = $connec->query($req);
+while ($row = $res->fetch(PDO::FETCH_OBJ)) {
+    if (!empty($row)) {
+
+        echo $row->name;
+
+        echo 'Vous êtes à présent connecté !';
     } else {
-        // on vérifie maintenant si le champ "Mot de passe" n'est pas vide"
-        if(empty($_POST['mdp'])) {
-            echo "Le champ Mot de passe est vide.";
-        } else {
-            // les champs sont bien posté et pas vide, on sécurise les données entrées par le membre:
-            $Pseudo = htmlentities($_POST['mail'], ENT_QUOTES, "ISO-8859-1"); // le htmlentities() passera les guillemets en entités HTML, ce qui empêchera les injections SQL
-            $MotDePasse = htmlentities($_POST['mdp'], ENT_QUOTES, "ISO-8859-1");
-            
-            //on se connecte à la base de données:
-            $mysqli = new PDO('mysql:host=127.0.0.1; dbname=db_acces_wifi', 'timyo', '123456');
-            
-            //on vérifie que la connexion s'effectue correctement:
-            if(!$mysqli){
-                echo "Erreur de connexion à la base de données.";
-            } else {
-                // on fait maintenant la requête dans la base de données pour rechercher si ces données existe et correspondent:
-                $Requete = "SELECT * FROM user WHERE mail = '";
-                $Requete .= $Pseudo;
-                $Requete .="' AND mdp = '";
-                $Requete .=$MotDePasse;
-                $Requete .="'";
-                // si il y a un résultat, mysqli_num_rows() nous donnera alors 1
-                // si mysqli_num_rows() retourne 0 c'est qu'il a trouvé aucun résultat
-                if(mysqli_num_rows($Requete) == 0) {
-                    echo "Le pseudo ou le mot de passe est incorrect, le compte n'a pas été trouvé.";
-                } else {
-                    // on ouvre la session avec $_SESSION:
-                    $_SESSION['mail'] = $Pseudo; // la session peut être appelée différemment et son contenu aussi peut être autre chose que le pseudo
-                    echo "Vous êtes à présent connecté !";
-                }
-            }
-        }
+        echo 'Vous êtes à tyer !';
     }
 }
-?>
+
+    }catch(PDOException $e){
+    echo 'requête plantée';
+}
+
